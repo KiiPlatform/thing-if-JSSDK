53da3280c691a1eeaed778f68cb37260092199b5
yongping <yongping.chen@kii.com>
2016-06-27 20:12:56 +0900

    add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack'),
+    path = require('path'),
+    yargs = require('yargs');
+ 
+var libraryName = 'ThingIF',
+    libraryFileName = 'thing-if-sdk',
+    plugins = [],
+    outputFile;
+ 
+if (yargs.argv.p) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }));
+  outputFile = libraryFileName + '.min.js';
+} else {
+  outputFile = libraryFileName + '.js';
+}
+ 
+var config = {
+  devtool: 'source-map',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [
+      { test: /\.tsx?$/, loader: 'ts', exclude: /node_modules/ }
+    ]
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: [ '', '.js', '.ts', '.jsx', '.tsx' ]
+  },
+  plugins: plugins
+ 
+};
+ 
+module.exports = config;
