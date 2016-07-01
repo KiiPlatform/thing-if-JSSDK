57a3f0347da1b69b1d2bc512cae59421ea5a6057
yongping <yongping.chen@kii.com>
2016-07-01 18:55:17 +0900

    fix test

--- test/siteTest.ts
+++ test/siteTest.ts
@@ -6,13 +6,13 @@ var thingIFSDK = require('../dist/thing-if-sdk.js');
 import {expect} from 'chai';
 var Site = thingIFSDK.Site;
 describe('Site', () => {
-    describe('#getBaseURL', () => {
-        it('base url string should be correct', () => {
-            expect(Site.getBaseUrl(Site.US)).to.equal("https://api.kii.com"); 
-            expect(Site.getBaseUrl(Site.JP)).to.equal("https://api-jp.kii.com"); 
-            expect(Site.getBaseUrl(Site.CN3)).to.equal("https://api-cn3.kii.com"); 
-            expect(Site.getBaseUrl(Site.SG)).to.equal("https://api-sg.kii.com"); 
-            expect(Site.getBaseUrl(Site.EU)).to.equal("https://api-eu.kii.com"); 
+    describe('#check enum', () => {
+        it('value of enum should be correct', () => {
+            expect(Site.US).to.equal(0); 
+            expect(Site.JP).to.equal(1); 
+            expect(Site.CN3).to.equal(2); 
+            expect(Site.SG).to.equal(3); 
+            expect(Site.EU).to.equal(4); 
         });
     });
 });
