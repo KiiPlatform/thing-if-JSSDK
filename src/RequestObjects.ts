bcd42bdc8e2ae3b61aab405f54d0c8083171296a
yongping <yongping.chen@kii.com>
2016-07-05 19:14:10 +0900

    use TypeID to apis

--- src/RequestObjects.ts
+++ src/RequestObjects.ts
@@ -7,48 +7,55 @@ import {TypeID} from './TypeID'
  * with vendorThingID with owner
  */
 export class OnboardWithVendorThingIDRequest {
-
+    public owner: string;
     constructor(
         public vendorThingID: string,
         public thingPassword: string,
-        public owner: TypeID,
+        ownerType: TypeID,
         public thingType?:string,
-        public thingProperties?:Object){}
+        public thingProperties?:Object){
+            this.owner = ownerType.toString();
+        }
 }
 
 /** OnboardWithThingIDRequest contains necessary fields to request onboarding
  * with thingID with owner
  */
 export class OnboardWithThingIDRequest {
-
+    public owner: string;
     constructor(
         public thingID: string,
         public thingPassword: string,
-        public owner: TypeID){}
+        ownerType: TypeID){
+            this.owner = ownerType.toString();
+        }
 }
 
 /** OnboardEndnodeWithGatewayRequest contains necessary fields to request onboarding
  * endnode with gateway
  */
 export class OnboardEndnodeWithGatewayRequest {
-
+    public owner: string;
     constructor(
         public gatewayThingID: string,
         public endNodeVendorThingID: string,
         public endNodePassword: string,
-        public owner: TypeID,
+        ownerType: TypeID,
         public thingType?:string,
-        public thingProperties?:Object){}
+        public thingProperties?:Object){
+            this.owner = ownerType.toString();
+        }
 }
 
 /** PostCommandRequest contains necessary fields to request post new command
  */
 export class PostCommandRequest {
+    public issuer: string;
     constructor(
         public schemaName: string,
         public schemaVersion: number,
         public actions: Object,
-        public issuer: TypeID,
+        issuerType: TypeID,
         public title?: string,
         public description?: string,
         public metaData?: Object){}
