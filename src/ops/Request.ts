e02b58da935ac1978d4164bc40c3de5ed5433648
nfukuzaki <noriyoshi.fukuzaki@kii.com>
2016-07-05 14:50:49 +0900

    checking http status

--- src/ops/Request.ts
+++ src/ops/Request.ts
@@ -5,24 +5,57 @@ import * as popsicle from 'popsicle';
 import {RequestOptions} from '~popsicle/dist/request';
 
 
+/**
+ * Perform an asynchronous HTTP request.
+ *
+ * @param {Object} options
+ *    <ul>
+ *      <li>method:The HTTP method to use for the request (e.g. "POST", "GET", "PUT").</li>
+ *      <li>url:A string containing the URL to which the request is sent.</li>
+ *      <li>body:Data to be sent to the server.</li>
+ *      <li>headers:An object of http header key/value pairs to send along with requests.</li>
+ *    </ul>
+ * @param {onCompletion} [onCompletion] callback function when completed
+ * @return {Promise} promise object 
+ */
 export default function (options: Object, onCompletion?: (err: Error, res: Object)=>void): Promise<Object>{
     return new Promise<Object>((resolve, reject) => {
-        //TODO: implment me
-        
         popsicle.request(<RequestOptions>options)
         .then(function (res) {
-            resolve(res);
-            if (!!onCompletion){
-                onCompletion(null, res);
+            if (isSucceeded(res.status)) {
+                resolve(res);
+                if (!!onCompletion){
+                    onCompletion(null, res);
+                }
+            } else {
+                var err: Error = new HttpRequestError(res.status, res.body);
+                reject(err);
+                if (!!onCompletion){
+                    onCompletion(err, null);
+                }
             }
         }).catch(function (err) {
-            reject({});
+            reject(err);
             if (!!onCompletion){
                 onCompletion(err, null);
             }
         });
-
-        //1. check status code
         //2. parse response
     })
+}
+function isSucceeded(status: number): boolean {
+    if (status >= 200 && status < 300) {
+        return true;
+    }
+    return false;
+}
+declare class BaseError implements Error {
+    public name: string;
+    public message: string;
+    constructor(message?: string);
+}
+class HttpRequestError extends BaseError {
+    constructor (status: number, message: string) {
+        super();    
+    }
 }
