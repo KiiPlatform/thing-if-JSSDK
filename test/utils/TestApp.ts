4c596a31efd7f419ad51ea394dc39eb730949119
yongping <yongping.chen@kii.com>
2016-07-05 16:56:39 +0900

    read env environment from typescript

--- test/utils/TestApp.ts
+++ test/utils/TestApp.ts
@@ -1,6 +1,22 @@
+// / <reference path="../../typings/globals/node/index.d.ts" />
+/// <reference path="../../typings/globals/mocha/index.d.ts" />
+/// <reference path="../../typings/globals/chai/index.d.ts" />
+import {expect} from 'chai';
+
 import {Site} from '../../src/Site'
-export const APPID = "****";
-export const APPKEY = "****";
-export const SITE = Site.JP;
+declare var process: any
+let envApp = (process.env.TestApp || "").split(":");
+
+describe('TestApp Configuration', () => {
+    describe('#check env string', () => {
+        it('should be configured', () => {
+            expect(envApp.length).to.equal(4); 
+        });
+    });
+});
+
+export const APPID = envApp[0];
+export const APPKEY = envApp[1];
+export const SITE = Site[envApp[2]];
 export const OWNERID = "34cc40051321-6169-4e11-017b-02367441";
-export const TOKEN = "****";
+export const TOKEN = envApp[3];
