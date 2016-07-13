b8514c2fcfbb08bb9f70ff4498623dace8903f1f
nfukuzaki <noriyoshi.fukuzaki@kii.com>
2016-07-13 10:04:47 +0900

    added small test for OnboardingOps

--- src/ops/OnboardingOps.ts
+++ src/ops/OnboardingOps.ts
@@ -3,18 +3,19 @@ import {Promise} from 'es6-promise';
 import request from './Request';
 import {App} from '../App';
 import {APIAuthor} from '../APIAuthor';
+import {OnboardingResult} from '../OnboardingResult'
 import * as utils from './OpsUtils';
 
 export function onboardWithThingID(
     au: APIAuthor, 
     onboardRequest: Object, 
-    onCompletion?: (err: Error, res:Object)=> void): Promise<Object> {
+    onCompletion?: (err: Error, res:OnboardingResult)=> void): Promise<OnboardingResult> {
     return onboard(au, "application/vnd.kii.OnboardingWithThingIDByOwner+json", onboardRequest, onCompletion);
 }
 export function onboardWithVendorThingID(
     au: APIAuthor, 
     onboardRequest:Object, 
-    onCompletion?: (err: Error, res:Object)=> void): Promise<Object> {
+    onCompletion?: (err: Error, res:OnboardingResult)=> void): Promise<OnboardingResult> {
     return onboard(au, "application/vnd.kii.OnboardingWithVendorThingIDByOwner+json", onboardRequest, onCompletion);
 }
 
@@ -22,24 +23,27 @@ function onboard(
     au: APIAuthor,
     contentType: string,
     onboardRequest: Object, 
-    onCompletion?: (err: Error, res:Object)=> void): Promise<Object> {
+    onCompletion?: (err: Error, res:OnboardingResult)=> void): Promise<OnboardingResult> {
     
     let onboardUrl = `${au.app.getThingIFBaseUrl()}/onboardings`;
-
-    return new Promise<Object>((resolve, reject) => {
+console.log("##### onboardUrl=" + onboardUrl);
+    return new Promise<OnboardingResult>((resolve, reject) => {
         var headers: any = utils.newHttpHeader(au);
         headers['Content-type'] = contentType;
+console.log("##### headers=" + JSON.stringify(headers));
         request({
             method: "POST",
             headers: headers,
             url: onboardUrl,
             body: onboardRequest,
         }).then((res:Object)=>{
-            resolve(res);
+console.log("##### res=" + JSON.stringify(res));
+            resolve(<OnboardingResult>res);
             if (!!onCompletion){
-                onCompletion(null, res);
+                onCompletion(null, <OnboardingResult>res);
             }
         }).catch((err:Error)=>{
+console.log("##### err=" + JSON.stringify(err));
             reject(err);
             if(!!onCompletion){
                 onCompletion(err, null);
---
+++ test/small/OnboardingOpsTest.ts
@@ -0,0 +1,105 @@
+/// <reference path="../../typings/globals/mocha/index.d.ts" />
+/// <reference path="../../typings/globals/chai/index.d.ts" />
+/// <reference path="../../typings/modules/nock/index.d.ts" />
+
+import {expect} from 'chai';
+import * as nock from 'nock'
+import {APIAuthor} from '../../src/APIAuthor';
+import {App} from '../../src/App';
+import {Site} from '../../src/Site';
+import {TypedID} from '../../src/TypedID';
+import {Types} from '../../src/TypedID';
+import * as RequestObjects from '../../src/RequestObjects';
+import * as OnboardingOps from '../../src/ops/OnboardingOps'
+import {OnboardingResult} from '../../src/OnboardingResult';
+let scope : nock.Scope;
+
+describe('OnboardingOps', function () {
+
+    let hostname = "http://api-jp.test.com";
+    let path = "/thing-if/apps/abcdefg/onboardings";
+    let appID = "abcd1234";
+    let appKey = "ABCD1234efgh5678IJKLM";
+    let ownerToken = "4qxjayegngnfcq3f8sw7d9l0e9fleffd";
+    let owner = new TypedID(Types.User, "userid-01234");
+    var au: APIAuthor;
+    let app = new App(appID, appKey, hostname);
+    // let requestOptions = {
+    //     method: "POST",
+    //     headers: {
+    //         "X-Kii-AppID": appID,
+    //         "X-Kii-AppKey": appKey,
+    //         "Authorization": ownerToken
+    //     },
+    //     url: `${hostname}${path}`
+    // };
+    let responseBody = {
+        accessToken: "p5Q9jtFdqRYTNgseurHLhQ0aehCYxAOJ2qzOO4TSKbw",
+        thingID: "th.7b3f20b00022-414b-6e11-0374-03ab0ce5",
+        mqttEndpoint: {
+            installationID: "roiczg4byahl2xji1fp2r1vk2",
+            username: "163fa051-p6QcV3cP5oFJjdEAZO6d2uC",
+            password: "jzgbQibmANOzaLLFkmnNCICbgHFYNyUBVlMREKHDbaHvmmWcmAGQlVYuoxQZAsQG",
+            mqttTopic: "obEHMF5Y3qeuAltpXjvSLnx",
+            host: "jp-mqtt-13915c439c5e.kii.com",
+            portTCP: 1883,
+            portSSL: 8883,
+            portWS: 12470,
+            portWSS: 12473,
+            ttl: 2147483647
+        }
+    }
+
+    beforeEach(function() {
+        nock.cleanAll();
+        au = new APIAuthor(ownerToken, app);
+    });
+    describe('#onboardWithThingID() with callback', function () {
+
+        let reqHeaders = {
+            "X-Kii-AppID": appID,
+            "X-Kii-AppKey": appKey,
+            "Authorization":"Bearer " + ownerToken,
+            "Content-Type": "application/vnd.kii.OnboardingWithThingIDByOwner+json"
+
+            ,"accept":"*/*","user-agent":"https://github.com/blakeembrey/popsicle","content-length":"108","accept-encoding":"gzip,deflate"
+        }
+
+        it("should send a request to the thing-if server", function (done) {
+            scope = nock(
+                hostname,
+                <any>{
+                    reqheaders: reqHeaders
+                }).post(path, {
+                    "thingID": "th.7b3f20b00022-414b-6e11-0374-03ab0ce5",
+                    "thingPassword": "password",
+                    "owner": owner.toString()
+                })
+                .reply(200, responseBody);
+            var request = new RequestObjects.OnboardWithThingIDRequest("th.7b3f20b00022-414b-6e11-0374-03ab0ce5", "password", owner);
+            OnboardingOps.onboardWithThingID(au, request, (err:Error, res:OnboardingResult)=>{
+                expect(err).not.be.null;
+                done();
+            });
+        });
+
+        // it("should handle error when thingID is null", function (done) {
+        // });
+        // it("should handle error when thingID is empty string", function (done) {
+        // });
+        // it("should handle error when password is null", function (done) {
+        // });
+        // it("should handle error when password is empty string", function (done) {
+        // });
+        // it("should handle error when server returned 403 error", function (done) {
+        // });
+        // it("should handle error when owner is null", function (done) {
+        // });
+        // it("should handle error when server returned 404 error", function (done) {
+        // });
+        // it("should handle error when server returned 405 error", function (done) {
+        // });
+    });
+    // describe('#onboardWithVendorThingID()', function () {
+    // });
+});
