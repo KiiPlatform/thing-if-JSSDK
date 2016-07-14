b98b492ac0ae10f053fa0a09dd756386bd92b4e7
yongping <yongping.chen@kii.com>
2016-07-14 18:19:09 +0900

    Add small tests

--- test/small/KiiPromiseTest.ts
+++ test/small/KiiPromiseTest.ts
@@ -9,32 +9,67 @@ declare var process: any
 
 class TestClass {
 
-    callbackSucceeded(onCompletion?: (err:Error, res: string)=>void): P<string>{
+    promiseSucceeded(onCompletion?: (err:Error, res: string)=>void): P<string>{
         let orgPromise = new P<string>((resolve, reject)=>{
             resolve("succeeded");
         });
         return promise(orgPromise, onCompletion);
     }
 
-    callbackFailed(onCompletion?: (err:Error, res: string)=>void): P<string>{
+    promiseFailed(onCompletion?: (err:Error, res: string)=>void): P<string>{
         let orgPromise = new P<string>((resolve, reject)=>{
             reject(new Error("failed"));
         });
         return promise(orgPromise, onCompletion);
     }
+
+    voidPromiseSucceeded(onCompletion?: (err:Error)=>void): P<void>{
+        let orgPromise = new P<void>((resolve, reject)=>{
+            resolve();
+        });
+        return voidPromise(orgPromise, onCompletion);
+    }
+
+    voidPromiseFailed(onCompletion?: (err:Error)=>void): P<void>{
+        let orgPromise = new P<void>((resolve, reject)=>{
+            reject(new Error("failed"));
+        });
+        return voidPromise(orgPromise, onCompletion);
+    }
 }
 
-describe("Test KiiPromise:", function () {
+describe("Test KiiPromise.promise:", function () {
 
-    it("callbacks, succeeded", function (done) {
+    it("callbacks and promise, succeeded", function (done) {
         let testObject = new TestClass();
         let callbackCalled = false;
-        testObject.callbackSucceeded((err, res)=>{
+        testObject.promiseSucceeded((err, res)=>{
             callbackCalled = true;
             expect(res).to.be.a('string');
             expect(err).to.null;
         }).then((res)=>{
             expect(callbackCalled).to.be.true;
+            expect(res).to.be.a('string');
+            done();
+        }).catch((err)=>{
+            done(err);
+        })
+    })
+
+    it("only callbacks, succeeded", function (done) {
+        let testObject = new TestClass();
+        let callbackCalled = false;
+        testObject.promiseSucceeded((err, res)=>{
+            callbackCalled = true;
+            expect(res).to.be.a('string');
+            expect(err).to.null;
+            done();
+        })
+    })
+
+    it("only promise, succeeded", function (done) {
+        let testObject = new TestClass();
+        testObject.promiseSucceeded().then((res)=>{
             expect(res).to.be.a('string');
             done();
         }).catch((err)=>{
@@ -52,7 +87,7 @@ describe("Test KiiPromise:", function () {
             done();
         });
         d.run(() => {
-            testObject.callbackSucceeded((err, res)=>{
+            testObject.promiseSucceeded((err, res)=>{
                 process.nextTick(()=>{
                     throw new Error();
                 })
@@ -62,10 +97,10 @@ describe("Test KiiPromise:", function () {
         });
     })
 
-    it("callbacks, failed", function (done) {
+    it("callbacks and promise, failed", function (done) {
         let testObject = new TestClass();
         let callbackCalled = false;
-        testObject.callbackFailed((err, res)=>{
+        testObject.promiseFailed((err, res)=>{
             callbackCalled = true;
             expect(err).to.be.a('error');
             expect(res).to.null;
@@ -74,6 +109,120 @@ describe("Test KiiPromise:", function () {
             done("should fail");
         }).catch((err)=>{
             expect(callbackCalled).to.be.true;
+            expect(err).to.be.a('error');
+            done();
+        }).catch((err)=>{
+            done(err);
+        })
+    })
+
+    it("only callbacks, failed", function (done) {
+        let testObject = new TestClass();
+        testObject.promiseFailed((err, res)=>{
+            expect(err).to.be.a('error');
+            expect(res).to.null;
+            done();
+        })
+    })
+
+    it("only promise, failed", function (done) {
+        let testObject = new TestClass();
+        testObject.promiseFailed().then((res)=>{
+            done("should fail");
+        }).catch((err)=>{
+            expect(err).to.be.a('error');
+            done();
+        }).catch((err)=>{
+            done(err);
+        })
+    })
+})
+
+describe("Test KiiPromise.voiPromise:", function () {
+
+    it("callbacks and promise, succeeded", function (done) {
+        let testObject = new TestClass();
+        let callbackCalled = false;
+        testObject.voidPromiseSucceeded((err)=>{
+            callbackCalled = true;
+            expect(err).to.null;
+        }).then(()=>{
+            expect(callbackCalled).to.be.true;
+            done();
+        }).catch((err)=>{
+            done(err);
+        })
+    })
+
+    it("only callbacks, succeeded", function (done) {
+        let testObject = new TestClass();
+        let callbackCalled = false;
+        testObject.voidPromiseSucceeded((err)=>{
+            callbackCalled = true;
+            expect(err).to.null;
+            done();
+        })
+    })
+
+    it("only promise, succeeded", function (done) {
+        let testObject = new TestClass();
+        testObject.voidPromiseSucceeded().then(()=>{
+            done();
+        }).catch((err)=>{
+            done(err);
+        })
+    })
+
+    it("callbacks(throw error), succeeded", function (done) {
+
+        const domain = require('domain');
+        const testObject = new TestClass();
+
+        const d = domain.create();
+        d.on('error', (er) => {
+            done();
+        });
+        d.run(() => {
+            testObject.voidPromiseSucceeded((err)=>{
+                process.nextTick(()=>{
+                    throw new Error();
+                })
+            }).catch((err)=>{
+                done("should not catched here");
+            })
+        });
+    })
+
+    it("callbacks and promise, failed", function (done) {
+        let testObject = new TestClass();
+        let callbackCalled = false;
+        testObject.voidPromiseFailed((err)=>{
+            callbackCalled = true;
+            expect(err).to.be.a('error');
+        }).then(()=>{
+            done("should fail");
+        }).catch((err)=>{
+            expect(callbackCalled).to.be.true;
+            expect(err).to.be.a('error');
+            done();
+        }).catch((err)=>{
+            done(err);
+        })
+    })
+
+    it("only callbacks, failed", function (done) {
+        let testObject = new TestClass();
+        testObject.voidPromiseFailed((err)=>{
+            expect(err).to.be.a('error');
+            done();
+        })
+    })
+
+    it("only promise, failed", function (done) {
+        let testObject = new TestClass();
+        testObject.voidPromiseFailed().then(()=>{
+            done("should fail");
+        }).catch((err)=>{
             expect(err).to.be.a('error');
             done();
         }).catch((err)=>{
