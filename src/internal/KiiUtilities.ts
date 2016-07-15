bc72ba1d51ccb9836056e83ed977807431be7c5c
yongping <yongping.chen@kii.com>
2016-07-15 17:54:39 +0900

    add KiiUtilities and tests

---
+++ src/internal/KiiUtilities.ts
@@ -0,0 +1,11 @@
+export function isString(mayStr: any): boolean {
+    return (typeof mayStr) == "string";
+}
+
+export function isNumber(mayNum: any): boolean {
+    return (typeof mayNum) == "number";
+}
+
+export function  isBoolean(mayBool:any): boolean {
+    return (typeof mayBool) == "boolean";
+}
---
+++ test/small/KiiUtilitiesTest.ts
@@ -0,0 +1,47 @@
+/// <reference path="../../typings/globals/mocha/index.d.ts" />
+/// <reference path="../../typings/globals/chai/index.d.ts" />
+import {expect} from 'chai';
+
+import * as KiiUtil from '../../src/internal/KiiUtilities'
+
+describe("Test KiiUtilities:", function() {
+    describe("test KiiUtilities#isNumber", function() {
+        it("should return true, when parameter is number", function() {
+            expect(KiiUtil.isNumber(3)).to.be.true;
+            expect(KiiUtil.isNumber(3.5)).to.be.true;
+        })
+        it("should return false, when parameter is not number", function() {
+            expect(KiiUtil.isNumber(new Date())).to.be.false;
+            expect(KiiUtil.isNumber("string")).to.be.false;
+            expect(KiiUtil.isNumber(true)).to.be.false;
+            expect(KiiUtil.isNumber({})).to.be.false;
+            expect(KiiUtil.isNumber(null)).to.be.false;
+        })
+    })
+
+    describe("test KiiUtilities#isString", function() {
+        it("should return true, when parameter is string", function() {
+            expect(KiiUtil.isString("")).to.be.true;
+            expect(KiiUtil.isString("a string")).to.be.true;
+        })
+        it("should return false, when parameter is not string", function() {
+            expect(KiiUtil.isString(new Date())).to.be.false;
+            expect(KiiUtil.isString(4.5)).to.be.false;
+            expect(KiiUtil.isString(true)).to.be.false;
+            expect(KiiUtil.isString({})).to.be.false;
+        })
+    })
+
+    describe("test KiiUtilities#isBoolean", function() {
+        it("should return true, when parameter is boolean", function() {
+            expect(KiiUtil.isBoolean(true)).to.be.true;
+            expect(KiiUtil.isBoolean(false)).to.be.true;
+        })
+        it("should return false, when parameter is not boolean", function() {
+            expect(KiiUtil.isBoolean(new Date())).to.be.false;
+            expect(KiiUtil.isBoolean("string")).to.be.false;
+            expect(KiiUtil.isBoolean(5.6)).to.be.false;
+            expect(KiiUtil.isBoolean({})).to.be.false;
+        })
+    })
+})
