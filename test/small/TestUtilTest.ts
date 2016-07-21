0415d05703bd7acb3178ca19746faf30b63d5d78
yongping <yongping.chen@kii.com>
2016-07-21 15:57:44 +0900

    imprv TestUtilTest

--- test/small/TestUtilTest.ts
+++ test/small/TestUtilTest.ts
@@ -8,14 +8,23 @@ describe("test TestUtil#sameDate", function() {
         var date1 = new Date();
         var date2 = new Date(date1.getTime()+1);
         var date3 = new Date(date1.getTime()+9);
+        var date4 = new Date(date1.getTime()-1);
+        var date5 = new Date(date1.getTime()-9);
+        expect(TestUtil.sameDate(date1, date1)).to.true;
         expect(TestUtil.sameDate(date1, date2)).to.true;
         expect(TestUtil.sameDate(date1, date3)).to.true;
+        expect(TestUtil.sameDate(date1, date4)).to.true;
+        expect(TestUtil.sameDate(date1, date5)).to.true;
     })
     it("if difference of 2 dates more than 10 milliseconds, they should not be same", function() {
         var date1 = new Date();
         var date2 = new Date(date1.getTime()+11);
         var date3 = new Date(date1.getTime()+19);
+        var date4 = new Date(date1.getTime()-11);
+        var date5 = new Date(date1.getTime()-19);
         expect(TestUtil.sameDate(date1, date2)).to.false;
         expect(TestUtil.sameDate(date1, date3)).to.false;
+        expect(TestUtil.sameDate(date1, date4)).to.false;
+        expect(TestUtil.sameDate(date1, date5)).to.false;
     })
 })
