132b2343ebb15923473faad59fa4ab5f67b71bff
yongping <yongping.chen@kii.com>
2016-07-21 13:17:41 +0900

    improve large tests of ThingIFAPI

--- test/large/ThingIFAPICommandTest.ts
+++ test/large/ThingIFAPICommandTest.ts
@@ -3,16 +3,18 @@
 import {expect} from 'chai';
 import {apiHelper, KiiUser, KiiThing} from './utils/APIHelper';
 import {testApp} from './utils/TestApp';
+import {Command, CommandState} from '../../src/Command'
+import {QueryResult} from '../../src/QueryResult'
 
 declare var require: any
 let thingIFSDK = require('../../../dist/thing-if-sdk.js');
 
-describe("Large Tests for Command APIs(ThingIFAPI):", function () {
+describe.only("Large Tests for Command APIs(ThingIFAPI):", function () {
 
     let user: KiiUser;
     let thingIFAPI: any;
 
-    before(function(done) {
+    beforeEach(function(done) {
         let au: any;
         apiHelper.createKiiUser().then((newUser: KiiUser) => {
             user = newUser;
@@ -31,7 +33,7 @@ describe("Large Tests for Command APIs(ThingIFAPI):", function () {
         })
     });
 
-    after(function(done) {
+    afterEach(function(done) {
         apiHelper.deleteKiiThing(thingIFAPI.target.id).then(()=>{
             return apiHelper.deleteKiiUser(user);
         }).then(()=>{
@@ -43,18 +45,59 @@ describe("Large Tests for Command APIs(ThingIFAPI):", function () {
 
     it("handle success response by calling ThingIFAPI#postNewCommand, ThingIFAPI#getCommand, and ThingIFAPI#listCommands", function (done) {
         var postCommandRequest = new thingIFSDK.PostCommandRequest("led", 1, [{turnPower: {power:true}}])
-        thingIFAPI.postNewCommand(postCommandRequest).then((cmd)=>{
+        var postCommandRequest2 = new thingIFSDK.PostCommandRequest("light", 2, [{turnPower: {power:false}}])
+        let command1:Command;
+        let command2:Command;
+        thingIFAPI.postNewCommand(postCommandRequest).then((cmd:Command)=>{
+            // test postNewCommand
             expect(cmd).not.null;
-            expect(cmd.commandID).not.equal("");
-            return thingIFAPI.postNewCommand(postCommandRequest);
-        }).then((cmd)=>{
-            expect(cmd).not.null;
-            expect(cmd.commandID).not.equal("");
+            expect(cmd.commandID).not.null;
+            expect(cmd.schemaName).to.be.equal(postCommandRequest.schema);
+            expect(cmd.schemaVersion).to.be.equal(postCommandRequest.schemaVersion);
+            expect(cmd.actions).to.be.deep.equal(postCommandRequest.actions);
+            expect(cmd.modified).not.null;
+            expect(cmd.created).not.null;
+            expect(!cmd.commandState).to.true;
+            expect(!cmd.title).to.be.true;
+            expect(!cmd.description).to.true;
+            expect(!cmd.metadata).to.true;
+            expect(!cmd.firedByTriggerID).to.true;
             return thingIFAPI.getCommand(cmd.commandID);
-        }).then(()=>{
+        }).then((cmd:Command)=>{
+            command1 = cmd;
+            // tet getCommand
+            expect(cmd).not.null;
+            expect(cmd.commandID).not.null;
+            expect(cmd.schemaName).to.be.equal(postCommandRequest.schema);
+            expect(cmd.schemaVersion).to.be.equal(postCommandRequest.schemaVersion);
+            expect(cmd.actions).to.be.deep.equal(postCommandRequest.actions);
+            expect(cmd.modified).not.null;
+            expect(cmd.created).not.null;
+            expect(cmd.commandState).to.equal(CommandState.SENDING);
+            expect(!cmd.title).to.be.true;
+            expect(!cmd.description).to.true;
+            expect(!cmd.metadata).to.true;
+            expect(!cmd.firedByTriggerID).to.true;
+            return thingIFAPI.postNewCommand(postCommandRequest2);
+        }).then((cmd:Command)=>{
+            return thingIFAPI.getCommand(cmd.commandID);
+        }).then((cmd:Command)=>{
+            command2 = cmd;
             return thingIFAPI.listCommands()
-        }).then((result)=>{
-            expect(result.results.length).to.equal(2);
+        }).then((result:QueryResult<Command>)=>{
+            // test listCommands
+            var commands = result.results;
+            expect(commands.length).to.equal(2);
+            for(var i in commands){
+                var cmd = commands[i];
+                if(cmd.commandID == command1.commandID){
+                    expect(cmd).to.deep.equal(command1);
+                }else{
+                    expect(cmd).to.deep.equal(command2);
+                }
+            }
+            expect(result.hasNext).to.be.false;
+            expect(!result.paginationKey).to.be.true;
             done();
         }).catch((err)=>{
             done(err);
