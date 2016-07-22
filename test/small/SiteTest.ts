2fee7db0d559e25eecdb8af6d38921e7a46e2cf4
nfukuzaki <noriyoshi.fukuzaki@kii.com>
2016-07-22 15:43:38 +0900

    fixed file name

---
+++ test/small/SiteTest.ts
@@ -0,0 +1,18 @@
+/// <reference path="../../typings/globals/mocha/index.d.ts" />
+/// <reference path="../../typings/globals/chai/index.d.ts" />
+declare var require: any
+var thingIFSDK = require('../../../dist/thing-if-sdk.js');
+
+import {expect} from 'chai';
+var Site = thingIFSDK.Site;
+describe('Site', () => {
+    describe('#check enum', () => {
+        it('value of enum should be correct', () => {
+            expect(Site.US).to.equal(0);
+            expect(Site.JP).to.equal(1);
+            expect(Site.CN3).to.equal(2);
+            expect(Site.SG).to.equal(3);
+            expect(Site.EU).to.equal(4);
+        });
+    });
+});
