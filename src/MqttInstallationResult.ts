6e7c2873202fcc1f18a4780d77709fba3ed8b250
nfukuzaki <noriyoshi.fukuzaki@kii.com>
2016-07-25 15:17:43 +0900

    added jsdoc

--- src/MqttInstallationResult.ts
+++ src/MqttInstallationResult.ts
@@ -1,8 +1,22 @@
 /** MqttInstallationResult represents result after installing Mqtt push notification to Kii Cloud succeeded. */
 export default class MqttInstallationResult {
+    /**
+     * ID used in IoT Cloud.
+     * @type {string}
+     */
     public installationID: string;
+    /**
+     * ID of registration that identifies the installation externally.
+     * @type {string}
+     */
     public installationRegistrationID: string;
 
+    /**
+     * Create a MqttInstallationResult.
+     * @constructor
+     * @param {string} installationID ID used in IoT Cloud.
+     * @param {string} installationRegistrationID ID of registration that identifies the installation externally.
+     */
     constructor(
         installationID: string,
         installationRegistrationID: string
--- src/OnboardingResult.ts
+++ src/OnboardingResult.ts
@@ -1,9 +1,28 @@
 /** OnboardingResult represents result after onboarding succeeded. */
-export class OnboardingResult{
+export class OnboardingResult {
+    /**
+     * D of the thing given by IoT Cloud.
+     * @type {string}
+     */
     public thingID: string;
+    /**
+     * Access token of the thing.
+     * @type {string}
+     */
     public accessToken: string;
+    /**
+     * MQTT installation details.
+     * @type {MqttEndpoint}
+     */
     public mqttEndPoint: MqttEndpoint;
 
+    /**
+     * Create a OnboardingResult.
+     * @constructor
+     * @param {string} thingID 
+     * @param {string} mqttEndPoint 
+     * @param {MqttEndpoint} mqttEndPoint 
+     */
     constructor(
         thingID: string,
         accessToken: string,
@@ -17,19 +36,58 @@ export class OnboardingResult{
 
 /** MqttEndpoint represents a MQTT endpoint */
 export class MqttEndpoint {
+    /**
+     * The ID of the installation.
+     * @type {string}
+     */
     public installationID: string;
+    /**
+     * The hostname of the MQTT broker host to connect.
+     * @type {string}
+     */
     public host: string;
+    /**
+     * The topic to subscribe in the MQTT broker.
+     * @type {string}
+     */
     public mqttTopic: string;
-    public userName: string;
+    /**
+     * The username to use for connecting to the MQTT broker.
+     * @type {string}
+     */
+    public username: string;
+    /**
+     * The password to use for connecting to the MQTT broker.
+     * @type {string}
+     */
     public password: string;
+    /**
+     * The port to connect using SSL/TLS.
+     * @type {number}
+     */
     public portSSL: number;
+    /**
+     * The port to connect using plain TCP.
+     * @type {number}
+     */
     public portTCP: number;
 
+    /**
+     * Create a MqttEndpoint.
+     * @constructor
+     * @param {string} installationID ID of the installation.
+     * @param {string} host The hostname of the MQTT broker host to connect.
+     * @param {string} mqttTopic The topic to subscribe in the MQTT broker.
+     * @param {string} username The username to use for connecting to the MQTT broker.
+     * @param {string} password The password to use for connecting to the MQTT broker.
+     * @param {number} portSSL The port to connect using SSL/TLS.
+     * @param {number} portTCP The port to connect using plain TCP.
+     */
     constructor(
         installationID: string,
         host: string,
         mqttTopic: string,
-        userName: string,
+        username: string,
         password: string,
         portSSL: number,
         portTCP: number
@@ -37,7 +95,7 @@ export class MqttEndpoint {
         this.installationID = installationID;
         this.host = host;
         this.mqttTopic = mqttTopic;
-        this.userName = userName;
+        this.username = username;
         this.password = password;
         this.portSSL = portSSL;
         this.portTCP = portTCP;
