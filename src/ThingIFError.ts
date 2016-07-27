4eaa799f416007444a9971d2f4b615d9943876d9
yongping <yongping.chen@kii.com>
2016-07-27 14:54:56 +0900

    imprv error handling

--- src/ThingIFError.ts
+++ src/ThingIFError.ts
@@ -1,33 +1,67 @@
+import * as KiiUtil from './internal/KiiUtilities'
 /** Represents types of error
 <ul>
     <li>Errors.ArgumentError: error caused by invalid arguments.</li>
     <li>Errors.HttpError: error caused when doing http request to kii server.</li>
     <li>Errors.IlllegalStateError: error caseud of illlegal states. </li>
+    <li>Errors.NetworkError: error caseud of network connection. </li>
 </ul>
 */
 export const Errors = {
     ArgumentError: "ArgumentError",
     HttpError: "HttpRequestError",
-    IlllegalStateError: "IlllegalStateError"
+    IlllegalStateError: "IlllegalStateError",
+    NetworkError: "NetworkError"
 }
 
+/** Represent error caught by thing-if sdk.
+ * @prop {string} name Name of ThingIFError. It is defined in {@link Errors}.
+ *  If it is not one of defined error, then it is unknown error.
+ * @prop {string} message Message of ThingIFError.
+*/
 export class ThingIFError {
     public name: string;
     public message: string;
     constructor(name: string, message:string) {
-        // Error.apply(this, arguments);
         this.name = name;
         this.message = message;
     }
 }
 ThingIFError.prototype = new Error();
 
+/** Represent body of error response from server.
+ * @prop {string} errorCode errorCode of response.
+ * @prop {string} message error message of response.
+ * @prop {string} rawData response body as string.
+*/
+export class ErrorResponseBody {
+    public errorCode: string;
+    public message: string;
+    public rawData: string;
+    constructor(errorCode: string, message: string, rawData: string){
+        this.errorCode = errorCode,
+        this.message = message;
+        this.rawData = rawData;
+    }
+}
+
+/** Represents http error.
+ * @prop {number} status Status code of error reponse.
+ * @prop {ErrorResponseBody} body ErrorResponseBody instance
+*/
 export class HttpRequestError extends ThingIFError {
     public status: number;
-    public body: Object;
-    constructor (status: number, message: string, body?: Object) {
+    public body: ErrorResponseBody;
+    constructor (status: number, message: string, body?: string|Object) {
         super(Errors.HttpError, message);
         this.status = status;
-        this.body = body;
+        if (KiiUtil.isObject(body)){
+            this.body = new ErrorResponseBody(
+                (<any>body).errorCode,
+                (<any>body).message,
+                JSON.stringify(body));
+        }else if(KiiUtil.isString(body)){
+            this.body = new ErrorResponseBody(null, null, <string>body)
+        }
     }
 }
