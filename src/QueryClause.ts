c867181c8713642bb4952b3d6663518a7c91057b
yongping <yongping.chen@kii.com>
2017-04-05 19:35:25 +0900

    Add QueryClause

--- src/QueryClause.ts
+++ src/QueryClause.ts
@@ -1,50 +1,57 @@
 /**
- * Base TriggerClause implementation.
+ * Base QueryClause implementation.
  */
-export abstract class TriggerClause {
+export abstract class QueryClause {
     abstract toJson(): any;
     /**
      * This method is for internal use only.
      * @param obj JSON object that represented a clause.
-     * @return {TriggerClause} Equals instance
+     * @return {QueryClause} a QueryClause instance
      */
-    static fromJson(obj:any): TriggerClause {
+    static fromJson(obj:any): QueryClause {
         if (obj.type == "eq") {
-            return EqualsClauseInTrigger.fromJson(obj);
+            return EqualsClauseInQuery.fromJson(obj);
         } else if (obj.type == "not") {
-            return NotEqualsClauseInTrigger.fromJson(obj);
+            return NotEqualsClauseInQuery.fromJson(obj);
         } else if (obj.type == "and") {
-            return AndClauseInTrigger.fromJson(obj);
+            return AndClauseInQuery.fromJson(obj);
         } else if (obj.type == "or") {
-            return OrClauseInTrigger.fromJson(obj);
+            return OrClauseInQuery.fromJson(obj);
         } else if (obj.type == "range") {
-            return RangeClauseInTrigger.fromJson(obj);
+            return RangeClauseInQuery.fromJson(obj);
+        } else if (obj.type == "all") {
+            return new AllClause();
         }
         return null;
     }
 }
 /**
- * Represents the clause of equals condition.
+ * Represents equals clause when query history states.
+ * @prop {string} alias alias name.
  * @prop {string} field Field name of comparison.
  * @prop {(string|number|boolean)} value Value to be compared.
  */
-export class EqualsClauseInTrigger extends TriggerClause {
+export class EqualsClauseInQuery extends QueryClause {
+    public alias: string;
     public field: string;
     public value: string|number|boolean;
 
     /**
-     * Create a equals condition.
+     * Create a equals clause.
      * @constructor
+     * @param {string} alias alias.
      * @param {string} field Field name of comparison.
      * @param {string} value Value to be compared.
      */
     constructor(
+        alias: string,
         field: string,
         value: string|number|boolean
     ) {
         super();
         this.field = field;
         this.value = value;
+        this.alias = alias;
     }
     /**
      * This method is for internal use only.
@@ -53,41 +60,48 @@ export class EqualsClauseInTrigger extends TriggerClause {
     toJson(): any {
         return {
             type: "eq",
+            alias: this.alias,
             field: this.field,
             value: this.value
         };
     }
     /**
      * This method is for internal use only.
-     * @param obj JSON object that represented a equals condition.
-     * @return {EqualsClauseInTrigger} Equals instance
+     * @param obj JSON object that represented a equals clause.
+     * @return {EqualsClauseInQuery} EqualsClauseInQuery instance
      */
-    static fromJson(obj:any): EqualsClauseInTrigger {
+    static fromJson(obj:any): EqualsClauseInQuery {
         let field = obj.field;
         let value = obj.value;
-        return new EqualsClauseInTrigger(field, value);
+        let alias = obj.alias;
+        return new EqualsClauseInQuery(alias, field, value);
     }
 }
 /**
- * Represents the clause of not equals condition.
+ * Represents not equals clause when query history states.
+ * @prop {string} alias alias.
  * @prop {string} field Field name of comparison.
  * @prop {(string|number|boolean)} value Value to be compared.
  */
-export class NotEqualsClauseInTrigger extends TriggerClause {
+export class NotEqualsClauseInQuery extends QueryClause {
+    public alias: string;
     public field: string;
     public value: string|number|boolean;
 
     /**
-     * Create a not equals condition.
+     * Create a not equals clause.
      * @constructor
+     * @param {string} alias alias.
      * @param {string} field Field name of comparison.
      * @param {string} value Value to be compared.
      */
     constructor(
+        alias: string,
         field: string,
         value: string|number|boolean
     ) {
         super();
+        this.alias = alias;
         this.field = field;
         this.value = value;
     }
@@ -100,6 +114,7 @@ export class NotEqualsClauseInTrigger extends TriggerClause {
             type: "not",
             clause: {
                 type: "eq",
+                alias: this.alias,
                 field: this.field,
                 value: this.value
             }
@@ -107,28 +122,29 @@ export class NotEqualsClauseInTrigger extends TriggerClause {
     }
     /**
      * This method is for internal use only.
-     * @param obj JSON object that represented a not equals condition.
-     * @return {NotEqualsClauseInTrigger} NotEquals instance
+     * @param obj JSON object that represented a not equals clause.
+     * @return {NotEqualsClauseInQuery} NotEqualsClauseInQuery instance
      */
-    static fromJson(obj:any): NotEqualsClauseInTrigger {
+    static fromJson(obj:any): NotEqualsClauseInQuery {
         let field = obj.clause.field;
         let value = obj.clause.value;
-        return new NotEqualsClauseInTrigger(field, value);
+        let alias = obj.clause.alias;
+        return new NotEqualsClauseInQuery(alias, field, value);
     }
 }
 /**
- * Represents the And operator.
- * @prop {TriggerClause[]} clauses Clauses to be concatenated with And operator.
+ * Represents the And clause when query history states.
+ * @prop {QueryClause[]} clauses Clauses to be concatenated with and clause.
  */
-export class AndClauseInTrigger extends TriggerClause {
-    public clauses: TriggerClause[];
+export class AndClauseInQuery extends QueryClause {
+    public clauses: QueryClause[];
 
     /**
      * Create a AND operator.
      * @constructor
-     * @param {TriggerClause[]} clauses Array of clauses to be concatenated with And operator.
+     * @param {QueryClause[]} clauses Array of clauses to be concatenated with And clause.
      */
-    constructor(...clauses: TriggerClause[]) {
+    constructor(...clauses: QueryClause[]) {
         super();
         this.clauses = clauses;
     }
@@ -138,7 +154,7 @@ export class AndClauseInTrigger extends TriggerClause {
      */
     toJson(): any {
         var json: any = {type: "and"};
-        var clauses :Array<TriggerClause> = new Array<TriggerClause>();
+        var clauses :Array<QueryClause> = new Array<QueryClause>();
         for (var clause of this.clauses) {
             clauses.push(clause.toJson());
         }
@@ -147,33 +163,33 @@ export class AndClauseInTrigger extends TriggerClause {
     }
     /**
      * This method is for internal use only.
-     * @param obj JSON object that represented a AND operator.
-     * @return {AndClauseInTrigger} And instance
+     * @param obj JSON object that represented a AND clause.
+     * @return {AndClauseInQuery} AndClauseInQuery instance
      */
-    static fromJson(obj:any): AndClauseInTrigger {
-        let clauses: Array<TriggerClause> = new Array<TriggerClause>();
+    static fromJson(obj:any): AndClauseInQuery {
+        let clauses: Array<QueryClause> = new Array<QueryClause>();
         let array: Array<any> = obj.clauses;
         for (var json of array) {
-            clauses.push(TriggerClause.fromJson(json));
+            clauses.push(QueryClause.fromJson(json));
         }
-        let and = new AndClauseInTrigger();
+        let and = new AndClauseInQuery();
         and.clauses = clauses;
         return and;
     }
 }
 /**
- * Represents the OR operator.
- * @prop {TriggerClause[]} clauses Clauses to be concatenated with Or operator.
+ * Represents the OR clause when query history states.
+ * @prop {QueryClause[]} clauses Clauses to be concatenated with Or clause.
  */
-export class OrClauseInTrigger extends TriggerClause {
-    public clauses: TriggerClause[];
+export class OrClauseInQuery extends QueryClause {
+    public clauses: QueryClause[];
 
     /**
      * Create a OR operator.
      * @constructor
-     * @param {TriggerClause[]} clauses Array of clauses to be concatenated with Or operator.
+     * @param {QueryClause[]} clauses Array of clauses to be concatenated with Or clause.
      */
-    constructor(...clauses: TriggerClause[]) {
+    constructor(...clauses: QueryClause[]) {
         super();
         this.clauses = clauses;
     }
@@ -183,7 +199,7 @@ export class OrClauseInTrigger extends TriggerClause {
      */
     toJson(): any {
         var json: any = {type: "or"};
-        var clauses :Array<TriggerClause> = new Array<TriggerClause>();
+        var clauses :Array<QueryClause> = new Array<QueryClause>();
         for (var clause of this.clauses) {
             clauses.push(clause.toJson());
         }
@@ -192,29 +208,31 @@ export class OrClauseInTrigger extends TriggerClause {
     }
     /**
      * This method is for internal use only.
-     * @param obj JSON object that represented a OR operator.
-     * @return {OrClauseInTrigger} Or instance
+     * @param obj JSON object that represented a OR clause.
+     * @return {OrClauseInQuery} Or instance
      */
-    static fromJson(obj:any): OrClauseInTrigger {
-        let clauses: Array<TriggerClause> = new Array<TriggerClause>();
+    static fromJson(obj:any): OrClauseInQuery {
+        let clauses: Array<QueryClause> = new Array<QueryClause>();
         let array: Array<any> = obj.clauses;
         for (var json of array) {
-            clauses.push(TriggerClause.fromJson(json));
+            clauses.push(QueryClause.fromJson(json));
         }
-        let or = new OrClauseInTrigger();
+        let or = new OrClauseInQuery();
         or.clauses = clauses;
         return or;
     }
 }
 /**
- * Represents the clause of range condition.
+ * Represents the clause of range clause when query history states.
+ * @prop {string} alias alias.
  * @prop {string} field Field name of comparison.
  * @prop {number} upperLimit The upper limit of the range.
  * @prop {boolean} upperIncluded Boolean field that indicates if the upper limit is contained in the range, if omitted is considered as "true".
  * @prop {number} lowerLimit The lower limit of the range.
  * @prop {boolean} lowerIncluded Boolean field that indicates if the lower limit is contained in the range, if omitted is considered as "true".
  */
-export class RangeClauseInTrigger extends TriggerClause {
+export class RangeClauseInQuery extends QueryClause {
+    public alias: string;
     public field: string;
     public upperLimit: number;
     public upperIncluded: boolean;
@@ -222,8 +240,9 @@ export class RangeClauseInTrigger extends TriggerClause {
     public lowerIncluded: boolean;
 
     /**
-     * Create a range condition.
+     * Create a range clause.
      * @constructor
+     * @param {string} alias alias.
      * @param {string} field Field name of comparison.
      * @param {number} upperLimit The upper limit of the range.
      * @param {boolean} upperIncluded Boolean field that indicates if the upper limit is contained in the range, if omitted is considered as "true".
@@ -231,6 +250,7 @@ export class RangeClauseInTrigger extends TriggerClause {
      * @param {boolean} lowerIncluded Boolean field that indicates if the lower limit is contained in the range, if omitted is considered as "true".
      */
     constructor(
+        alias: string,
         field: string,
         upperLimit: number,
         upperIncluded: boolean,
@@ -238,6 +258,7 @@ export class RangeClauseInTrigger extends TriggerClause {
         lowerIncluded: boolean
     ) {
         super();
+        this.alias = alias;
         this.field = field;
         this.upperLimit = upperLimit;
         this.upperIncluded = upperIncluded;
@@ -246,35 +267,43 @@ export class RangeClauseInTrigger extends TriggerClause {
     }
     /**
      * Create a Range instance of the less than.
+     * @param {string} alias alias.
      * @param {string} field Field name of comparison.
      * @param {number} lowerLimit The upper lower of the range.
+     * @returns {RangeClauseInQuery} RangeClauseInQuery instance.
      */
-    static greaterThan(field: string, lowerLimit: number): RangeClauseInTrigger {
-        return new RangeClauseInTrigger(field, null, null, lowerLimit, false);
+    static greaterThan(alias: string, field: string, lowerLimit: number): RangeClauseInQuery {
+        return new RangeClauseInQuery(alias, field, null, null, lowerLimit, false);
     }
     /**
      * Create a Range instance of the less than or equals.
+     * @param {string} alias alias.
      * @param {string} field Field name of comparison.
      * @param {number} lowerLimit The upper lower of the range.
-     */
-    static greaterThanEquals(field: string, lowerLimit: number): RangeClauseInTrigger {
-        return new RangeClauseInTrigger(field, null, null, lowerLimit, true);
+     * @returns {RangeClauseInQuery} RangeClauseInQuery instance.
+    */
+    static greaterThanEquals(alias: string, field: string, lowerLimit: number): RangeClauseInQuery {
+        return new RangeClauseInQuery(alias, field, null, null, lowerLimit, true);
     }
     /**
      * Create a Range instance of the greater than.
+     * @param {string} alias alias.
      * @param {string} field Field name of comparison.
      * @param {number} upperLimit The upper limit of the range.
+     * @returns {RangeClauseInQuery} RangeClauseInQuery instance.
      */
-    static lessThan(field: string, upperLimit: number): RangeClauseInTrigger {
-        return new RangeClauseInTrigger(field, upperLimit, false, null, null);
+    static lessThan(alias: string, field: string, upperLimit: number): RangeClauseInQuery {
+        return new RangeClauseInQuery(alias, field, upperLimit, false, null, null);
     }
     /**
      * Create a Range instance of the greater than or equals.
+     * @param {string} alias alias.
      * @param {string} field Field name of comparison.
      * @param {number} upperLimit The upper limit of the range.
+     * @returns {RangeClauseInQuery} RangeClauseInQuery instance.
      */
-    static lessThanEquals(field: string, upperLimit: number): RangeClauseInTrigger {
-        return new RangeClauseInTrigger(field, upperLimit, true, null, null);
+    static lessThanEquals(alias: string, field: string, upperLimit: number): RangeClauseInQuery {
+        return new RangeClauseInQuery(alias, field, upperLimit, true, null, null);
     }
     /**
      * This method is for internal use only.
@@ -294,19 +323,37 @@ export class RangeClauseInTrigger extends TriggerClause {
         if (this.lowerIncluded != null && this.lowerIncluded != undefined) {
             json["lowerIncluded"] = this.lowerIncluded;
         }
+        json["alias"] = this.alias;
         return json;
     }
     /**
      * This method is for internal use only.
      * @param obj JSON object that represented a range condition.
-     * @return {RangeClauseInTrigger} Range instance
+     * @return {RangeClauseInQuery} RangeClauseInQuery instance
      */
-    static fromJson(obj:any): RangeClauseInTrigger {
+    static fromJson(obj:any): RangeClauseInQuery {
         let field = obj.field;
         let upperLimit = obj.upperLimit ? obj.upperLimit : null;
         let upperIncluded = obj.upperIncluded ? obj.upperIncluded : null;
         let lowerLimit = obj.lowerLimit ? obj.lowerLimit : null;
         let lowerIncluded = obj.lowerIncluded ? obj.lowerIncluded : null;
-        return new RangeClauseInTrigger(field, upperLimit, upperIncluded, lowerLimit, lowerIncluded);
+        let alias = obj.alias;
+        return new RangeClauseInQuery(alias, field, upperLimit, upperIncluded, lowerLimit, lowerIncluded);
+    }
+}
+
+/**
+ * Represents all clause.
+ */
+export class AllClause extends QueryClause {
+    
+    /**
+     * This method is for internal use only.
+     * @return {Object} JSON object that represented this instance.
+     */
+    toJson(): Object {
+        return {
+            type: "all"
+        }
     }
 }
