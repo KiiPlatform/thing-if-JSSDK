be6bd062ecc7893e3b9837d51c077f4f7556ae06
yongping <yongping.chen@kii.com>
2017-05-09 19:10:07 +0900

    improve small tests

--- test/small/APIAuthorStateTest.ts
+++ test/small/APIAuthorStateTest.ts
@@ -18,12 +18,12 @@ let au = new APIAuthor("dummy-token", testApp.app);
 let target = new TypedID(Types.Thing, "dummy-thing-id");
 
 
-describe("Test APIAuthor#getState", function() {
+describe("Test APIAuthor#getState without alias", function() {
     let commandID = "1234235534ferw"
 
     describe("handle succeeded reponse", function() {
 
-        let expectedState = {power: true};
+        let expectedState = {alias1: {power: true}};
 
         beforeEach(function() {
             simple.mock(StateOps.prototype, 'getState').returnWith(
@@ -45,7 +45,7 @@ describe("Test APIAuthor#getState", function() {
             })
         })
         it("test callback", function (done) {
-            au.getState(target,(err, state)=>{
+            au.getState(target,null, (err, state)=>{
                 try{
                     expect(err).to.null;
                     expect(state).to.be.deep.equal(expectedState);
@@ -83,7 +83,85 @@ describe("Test APIAuthor#getState", function() {
             })
         })
         it("test callback", function (done) {
-            au.getState(target,(err, state)=>{
+            au.getState(target, null, (err, state)=>{
+                try{
+                    expect(err).to.be.deep.equal(expectedError);
+                    expect(state).to.null;
+                    done();
+                }catch(err){
+                    done(err);
+                }
+            })
+        })
+    })
+})
+
+describe("Test APIAuthor#getState with alias", function() {
+    let commandID = "1234235534ferw"
+
+    describe("handle succeeded reponse", function() {
+
+        let expectedState = {power: true};
+
+        beforeEach(function() {
+            simple.mock(StateOps.prototype, 'getState').returnWith(
+                new P<Object>((resolve, reject)=>{
+                    resolve(expectedState);
+                })
+            );
+        })
+        afterEach(function() {
+            simple.restore();
+        })
+        it("test promise", function (done) {
+            au.getState(target, "alias1")
+            .then((state)=>{
+                expect(state).to.be.deep.equal(expectedState);
+                done();
+            }).catch((err)=>{
+                done(err);
+            })
+        })
+        it("test callback", function (done) {
+            au.getState(target,"alias1", (err, state)=>{
+                try{
+                    expect(err).to.null;
+                    expect(state).to.be.deep.equal(expectedState);
+                    done();
+                }catch(err){
+                    done(err);
+                }
+            })
+        })
+    })
+
+    describe("handle err reponse", function() {
+        let expectedError = new HttpRequestError(404, Errors.HttpError, {
+            "errorCode": "TARGET_NOT_FOUND",
+            "message": `Target thing:${target.id} not found`
+        });
+
+        beforeEach(function() {
+            simple.mock(StateOps.prototype, 'getState').returnWith(
+                new P<Object>((resolve, reject)=>{
+                    reject(expectedError);
+                })
+            );
+        })
+        afterEach(function() {
+            simple.restore();
+        })
+        it("test promise", function (done) {
+            au.getState(target, "alias1")
+            .then((state)=>{
+                done("should fail");
+            }).catch((err: HttpRequestError)=>{
+                expect(err).to.be.deep.equal(expectedError);
+                done();
+            })
+        })
+        it("test callback", function (done) {
+            au.getState(target, "alias1", (err, state)=>{
                 try{
                     expect(err).to.be.deep.equal(expectedError);
                     expect(state).to.null;
