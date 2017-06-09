e6c73f8091909a91fd7eb0e606b53f8bf45b1258
yongping <yongping.chen@kii.com>
2017-06-09 19:10:41 +0900

    update version

--- test/small/utils/TestUtil.ts
+++ test/small/utils/TestUtil.ts
@@ -2,5 +2,5 @@ export function sameDate(date1: Date, date2: Date): boolean{
     return Math.abs(date1.getTime()-date2.getTime()) < 10;
 }
 export function sdkVersion(): string {
-    return '1.0.0';
+    return '1.0.2';
 }
